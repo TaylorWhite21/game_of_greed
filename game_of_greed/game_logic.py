ea4516cf886f9fd308c278bf4492a0f6b3dab589
Taylor <taylorwhite21@yahoo.com>
2021-12-30 00:19:25 -0500

    Added logic for starting game and rolling x number of dice

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,7 +1,78 @@
 import random
 from collections import Counter
+from banker import Banker
+
+# num_of_dice = 6
+# rounds = 0
 
 class Gamelogic:
+  
+  def __init__(self):
+    self.num_of_dice = 6
+    self.rounds = 0
+    self.saved_dice = ''
+    self.dice_list = []
+    self.banker = Banker()
+    
+  def play_game(self): 
+    print('Welcome to Game of Greed')
+    print('(y)es to play or (n)o to decline')
+    answer = input('> ')
+    if answer == 'y':
+      self.start_new_round()
+    
+    print('Enter dice to keep, or (q)uit:')
+    self.saved_dice = input('> ')
+    if self.saved_dice == 'q':
+     print('Thanks for playing.')
+    else:
+      print('seriously, this hurts')
+      print(self.dice_list)
+      self.dice_list = self.string_to_integer(self.saved_dice)
+      print(self.dice_list)
+      for dice in self.dice_list:
+        print('this is hell')
+        if self.saved_dice == self.dice_list[dice]:
+          self.banker.shelf = self.calculate_score(self.saved_dice)
+          unbanked_points = self.banker.shelf
+          print(f'You have {unbanked_points} unbanked points')
+    
+    print('(r)oll again, (b)ank your points or (q)uit:')
+    answer_after_roll = input('> ')
+    if answer_after_roll == 'r':
+     self.start_new_round()
+    
+    elif answer_after_roll == 'b':
+      print('Please make it stop')
+      self.banker.bank()
+      
+    else:
+      print('Thanks for playing.')
+    
+  def start_new_round(self):
+    roll_str = ''
+    self.rounds += 1
+    print(f'Starting round {self.rounds}')
+    how_many_dice = self.number_of_rolled_dice()
+    roll_results = self.roll_dice(how_many_dice)
+    for num in roll_results:
+      roll_str += str(num) + ' '
+    print(f'*** {roll_results}***')
+  
+  def number_of_rolled_dice(self):
+    
+    rolled_dice = self.num_of_dice - len(self.string_to_integer(self.saved_dice))
+    return rolled_dice
+    
+  def string_to_integer(self, str):
+    saved_dice_list = []
+    for char in str:
+      try:
+        integer = int(char)
+        saved_dice_list.append(integer)
+      except:
+        continue
+    return saved_dice_list
   
   @staticmethod
   def roll_dice(rolled_dice):
@@ -38,3 +109,7 @@ class Gamelogic:
 
     print("return", points)
     return points
+
+if __name__ == '__main__':
+  play_game = Gamelogic()
+  play_game.play_game()
